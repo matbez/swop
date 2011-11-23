f047e633eb24baa5d1d9297dee266c34a6c69073
matbez <matbez@wp.pl>
2011-11-23 01:49:48 +0100

    Update klient2.py

--- klient2.py
+++ klient2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Program Klient
 do wymiany danych medycznych z uzyciem standardu HL7
@@ -5,49 +6,65 @@ w ramach projektu SWOP
 oraz pracy inzynierskiej
 Mateusz Bezkorowajny
 '''
-
 import sys
-import gevent
 import _socket
 from gevent.socket import socket
 from gevent.socket import create_connection
-from gevent.socket import tcp_listener
-from gevent import monkey
 from gevent.ssl import SSLSocket
 
 HOST = '127.0.0.1'
 PORT = 1234
-#BUFSIZ = 1024
 address = (HOST, PORT)
 blad = None
 
-
-def main():  
-  gniazdo = None
-  try:
-    gniazdo = create_connection(address) 
-    print 'Udalo sie polaczyc z:', HOST, 'do portu:', PORT
-  except _socket.error, blad:
-    print 'wystapil blad:' 
-    print blad 
-  while True:
-    msg = gniazdo.recv(1024)  
-    print msg #wstepne informacje z serwera
-    msg = raw_input() #odpowiedz od uzyszkodnika
-    print 'wyslales ', msg
-    gniazdo.send(msg)
-    msg = gniazdo.recv(1024)
-    print msg
-    if msg:          
-        msg = raw_input()
-        gniazdo.send(msg)
-        continue
+def wprowadz_dane():
+    n='0'
+    dane=raw_input() + '\r'
+    while True:
+        n = raw_input() + '\r'
+        if n == '\r':
+            return(dane)
+        else:
+            dane+= n
+        
+def wyslij_dane(socket, dane):
+    dlugosc=str(len(dane))
+    socket.send(dlugosc)
+    ack=socket.recv(3)
+    ack = int(ack)
+    if ack == 1:
+        socket.send(dane)
     else:
-        print 'Zakonczyles polaczenie z serwerem.'
-        break
+        print "Mamy problem - serwer nie odpowiada na wiadomosc o dlugosci danych!"
+        
+def odbierz_dane(socket):
+    rozmiar = int(socket.recv(1024))
+    if rozmiar != 0:
+        socket.send("1")
+        dane = socket.recv(rozmiar)
+        return(dane)
+    else:
+        socket.send("0")
+        print "Problem z odbiorem danych!"
 
+def main():
+    socket = None
+    try:
+        socket = create_connection(address)
+        print 'Udalo sie polaczyc z:', HOST, 'do portu:', PORT
+    except _socket.error, blad:
+        print 'wystapil blad:' 
+        print blad 
+    while True:
+        try:
+            dane=odbierz_dane(socket)
+            print dane
+            #Klient odpowiada:
+            dane = wprowadz_dane()
+            wyslij_dane(socket, dane)
+        except:
+            print "Polaczenie zostalo zerwane!"   
+            break 
+    
 if __name__ == '__main__':
-  sys.exit(main())
-
-
-
+    sys.exit(main())
